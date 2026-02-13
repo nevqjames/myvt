73e4e85d7254ddca09798af2f16e02eb29455282
nevqjames <nevqjames@gmail.com>
2026-02-13 16:07:20 +0800

    Logout mobile

--- asset/js/admin.js
+++ asset/js/admin.js
@@ -50,5 +50,6 @@ window.addEventListener('keydown', (e) => {
 if (urlParams.has('mod')) tryLogin();
 if (urlParams.has('logout')) {
     localStorage.removeItem('adminKey');
-    location.reload();
+    // Redirect to current page without logout param
+    window.location.href = window.location.href.split('?')[0];
 }
